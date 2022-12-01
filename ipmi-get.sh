a0956bf62d10bffe9e081cc4718346932655e857
Serge <shubinov@gmail.com>
2022-12-01 23:04:38 +0500

    tune for T41S nodes

--- ipmi-get.sh
+++ ipmi-get.sh
@@ -1,3 +1,16 @@
-ipmi-fru -e 03 -h 192.168.100.19 -u root -p changeme
-ipmi-sensors -h 192.168.100.19 -u root -p changeme
-ipmi-sel -D LAN -h 192.168.100.19 -u root -p changeme --output-event-state
+bmc_ip=("192.168.10.1" "192.168.10.2")
+uname="root"
+pswd="*****"
+
+for i in ${bmc_ip[@]}
+do
+  # chassis SN, node SN(FRU Product Serial Number:_), node(product) model name, node(product) model pn
+  mkdir $i
+  ipmi-fru -e 0 -h $i -u $uname -p $pswd # for T41S -e 00h > $i/ipmi-fru.out
+  # bmc version, BIOS version
+  bmc-info -h $i -u $uname -p $pswd > $i/bmc-info.out
+  #sensors
+  ipmi-sensors -h $i -u $uname -p $pswd $i/ipmi-sensors.out
+  #SEL
+  ipmi-sel -D LAN -h $i -u $uname -p $pswd --output-event-state > $i/ipmi-sel.out
+done
